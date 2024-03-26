4dfa7d09b8c7ab0788e26a06ceed24fb094fba5f
Gabriel Martin <g.martin@htlkrems.at>
2024-03-26 07:32:10 +0100

    Create Webscraper project

---
+++ MPM-Betting.Scraper/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+WebDriver driver = new ChromeDriver();
+driver.Navigate().GoToUrl("https://www.google.at");
+var element = driver.FindElement(By.TagName("h1"));
+Console.WriteLine(element.Text);
