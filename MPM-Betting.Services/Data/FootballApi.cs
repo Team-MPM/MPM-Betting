70676723810ccd86793890a0827cbe6395122036
Gabriel Martin <g.martin@htlkrems.at>
2024-04-13 21:21:26 +0200

    Refactor API mapping and abstract caching

--- MPM-Betting.Services/Data/FootballApi.cs
+++ MPM-Betting.Services/Data/FootballApi.cs
@@ -10,54 +10,47 @@ public static class FootballApi
     
     public static WebApplication MapFootballEndpoints(this WebApplication app)
     {
-        app.MapGet("/football/leagues", async (IMemoryCache cache) =>
-            {
-                const string cacheKey = "footballLeagues";
-                if (cache.TryGetValue(cacheKey, out List<LeagueEntry>? allLeagues)) 
-                    return allLeagues;
-
-                allLeagues ??= [];
-                
-                var client = new HttpClient();
-                var response = await client.GetAsync("https://www.fotmob.com/api/allLeagues");
-
-                var json = await response.Content.ReadAsStringAsync();
-                var jObject = JObject.Parse(json);
-                
-                var international = (JArray)jObject["international"]!;
-                var internationalLeagues = international[0]["leagues"]!;
-                
-                foreach (var league in internationalLeagues)
-                {
-                    var leagueName = league["name"]!.Value<string>()!;
-                    var leagueId = league["id"]!.Value<int>();
-                    allLeagues.Add(new LeagueEntry(leagueName, "international", leagueId));
-                }
-                
-                var countries = (JArray)jObject["countries"]!;
-                
-                foreach (var jToken in countries)
-                {
-                    var countryName = jToken["name"]!.Value<string>()!;
-                    var leagues = (JArray)jToken["leagues"]!;
-                    foreach (var league in leagues)
-                    {
-                        var leagueName = league["name"]!.Value<string>()!;
-                        var leagueId = league["id"]!.Value<int>();
-                        allLeagues.Add(new LeagueEntry(leagueName, countryName, leagueId));
-                    }
-                }
-                
-                var cacheEntryOptions = new MemoryCacheEntryOptions()
-                    .SetSlidingExpiration(TimeSpan.FromDays(1));
-
-                cache.Set(cacheKey, allLeagues, cacheEntryOptions);
-                
-                return allLeagues;
-            })
-        .WithName("GetAllFootballLeagues")
-        .WithOpenApi();
+        app.MapGet("/football/leagues", async (IMemoryCache cache) => await Utils.GetViaCache(cache, TimeSpan.FromDays(1), "footballLeagues", GetAllFootballLeagues))
+            .WithName("GetAllFootballLeagues")
+            .WithOpenApi();
         
         return app;
+    }
+
+    private static async Task<List<LeagueEntry>> GetAllFootballLeagues()
+    {
+        var allLeagues = new List<LeagueEntry>();
+
+        var client = new HttpClient();
+        var response = await client.GetAsync("https://www.fotmob.com/api/allLeagues");
+
+        var json = await response.Content.ReadAsStringAsync();
+        var jObject = JObject.Parse(json);
+
+        var international = (JArray)jObject["international"]!;
+        var internationalLeagues = international[0]["leagues"]!;
+
+        foreach (var league in internationalLeagues)
+        {
+            var leagueName = league["name"]!.Value<string>()!;
+            var leagueId = league["id"]!.Value<int>();
+            allLeagues.Add(new LeagueEntry(leagueName, "international", leagueId));
+        }
+
+        var countries = (JArray)jObject["countries"]!;
+
+        foreach (var jToken in countries)
+        {
+            var countryName = jToken["name"]!.Value<string>()!;
+            var leagues = (JArray)jToken["leagues"]!;
+            foreach (var league in leagues)
+            {
+                var leagueName = league["name"]!.Value<string>()!;
+                var leagueId = league["id"]!.Value<int>();
+                allLeagues.Add(new LeagueEntry(leagueName, countryName, leagueId));
+            }
+        }
+
+        return allLeagues;
     }
 }
---
+++ MPM-Betting.Services/Data/Utils.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace MPM_Betting.Services.Data;
+
+public partial class Utils
+{
+    public static T GetViaCache<T>(IMemoryCache cache,  TimeSpan expiration, string cacheKey, Func<T> generator)
+    {
+        if (cache.TryGetValue(cacheKey, out T? cachedValue))
+        {
+            return cachedValue!;
+        }
+
+        var value = generator.Invoke();
+        
+        var cacheEntryOptions = new MemoryCacheEntryOptions()
+            .SetSlidingExpiration(expiration);
+
+        cache.Set(cacheKey, value, cacheEntryOptions);
+        
+        return value;
+    }
+    
+    public static async Task<T> GetViaCache<T>(IMemoryCache cache, TimeSpan expiration, string cacheKey, Func<Task<T>> generator)
+    {
+        if (cache.TryGetValue(cacheKey, out T? cachedValue))
+        {
+            return cachedValue!;
+        }
+
+        var value = await generator.Invoke();
+        
+        var cacheEntryOptions = new MemoryCacheEntryOptions()
+            .SetSlidingExpiration(expiration);
+
+        cache.Set(cacheKey, value, cacheEntryOptions);
+        
+        return value;
+    }
+}
