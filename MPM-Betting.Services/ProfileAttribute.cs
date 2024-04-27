0ee91ad920cc86b0364cbddc36afd3db8deb1ccb
Gabriel Martin <g.martin@htlkrems.at>
2024-04-27 09:21:24 +0200

    Add ProfileAttribute using PostSharp

---
+++ MPM-Betting.Services/ProfileAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using PostSharp.Aspects;
+using PostSharp.Extensibility;
+using PostSharp.Serialization;
+
+namespace MPM_Betting.Services;
+
+[PSerializable]
+[MulticastAttributeUsage(MulticastTargets.Method)]
+public class ProfileAttribute : MethodInterceptionAspect
+{
+    public override void OnInvoke(MethodInterceptionArgs args)
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        args.Proceed();
+        stopwatch.Stop();
+        Console.WriteLine($"{args.Method.Name} took: {stopwatch.Elapsed}");
+    }
+}
