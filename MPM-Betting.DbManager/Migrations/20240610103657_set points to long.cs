fff340b6dae0feef898c9a65ecbfbff828d9895a
Gabriel Martin <g.martin@htlkrems.at>
2024-06-10 13:03:35 +0200

    Make point a long - fixes the arithmetic integer wraparound once you get a lot of points

---
+++ MPM-Betting.DbManager/Migrations/20240610103657_set points to long.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MPM_Betting.DbManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class setpointstolong : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bets_Games_GameId",
+                table: "Bets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_AspNetUsers_TargetId",
+                table: "Notifications");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TargetId",
+                table: "Notifications",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GameId",
+                table: "Bets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Points",
+                table: "AspNetUsers",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bets_Games_GameId",
+                table: "Bets",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_AspNetUsers_TargetId",
+                table: "Notifications",
+                column: "TargetId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bets_Games_GameId",
+                table: "Bets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_AspNetUsers_TargetId",
+                table: "Notifications");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TargetId",
+                table: "Notifications",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GameId",
+                table: "Bets",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Points",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bets_Games_GameId",
+                table: "Bets",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_AspNetUsers_TargetId",
+                table: "Notifications",
+                column: "TargetId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
